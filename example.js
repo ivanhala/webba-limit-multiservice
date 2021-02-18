f6970d80b6c43eb2eda3956f2cad8bb7ff458a3d
Ivan <ivan@plugion.org>
2021-02-18 12:34:28 +0200

    Create example.js

---
+++ example.js
@@ -0,0 +1,12 @@
+jQuery( function ($) {	 
+	if( jQuery('#wbk-confirm-services').length > 0 ){
+		jQuery( '.wbk-service-checkbox' ).change( function(){         
+			var service_count = jQuery( '.wbk-service-checkbox:checked').length;
+		 	if( service_count != 2 ){
+		 		jQuery( '#wbk-confirm-services' ).attr( "disabled", true );
+		 	} else {
+		 		jQuery( '#wbk-confirm-services' ).attr( "disabled", false );
+		 	}
+		});		 
+	}
+});
