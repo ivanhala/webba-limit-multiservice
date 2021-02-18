9476a621e4a5362369ab34b27940f639f1e27a07
Ivan <ivan@plugion.org>
2021-02-18 12:35:29 +0200

    Update

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 jQuery( function ($) {	 
 	if( jQuery('#wbk-confirm-services').length > 0 ){
-		jQuery( '.wbk-service-checkbox' ).change( function(){         
+		jQuery( '.wbk-service-checkbox'  ).change( function(){         
 			var service_count = jQuery( '.wbk-service-checkbox:checked').length;
 		 	if( service_count != 2 ){
 		 		jQuery( '#wbk-confirm-services' ).attr( "disabled", true );
